88609271bf9cafa434e74f464570c1ec507a948b
eriklimamolykote <eriklim@hotmail.com>
2022-02-28 15:47:15 -0300

    commit-projetos-react-28-02-2022

--- pizza-react/src/App.js
+++ pizza-react/src/App.js
@@ -1,6 +1,10 @@
-import './App.css';
-import NavBar from './components/NavBar.js';
-import Home from './pages/Home.js';
+import "./App.css";
+import NavBar from "./components/NavBar.js";
+import Footer from "./components/Footer";
+import Home from "./pages/Home.js";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
@@ -9,7 +13,10 @@ function App() {
       <Router>
         <NavBar />
         <Routes>
-            <Route path="/" exact component={Home}/>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/menu" element={<Menu />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
         </Routes>
       </Router>
     </div>
