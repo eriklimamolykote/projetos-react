0ad8d6cfa4e9882536dc7ccc8d2b05aed6a00627
eriklimamolykote <eriklim@hotmail.com>
2022-03-02 11:08:03 -0300

    primeiro-commit-projetos-react-02-03-2022

---
+++ todo-project/src/index.js
@@ -0,0 +1,26 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+//component
+import TodoContainer from "./functionBased/components/TodoContainer"
+
+//stylesheet
+import "./functionBased/App.css"
+
+ReactDOM.render(
+
+  <React.StrictMode>
+
+    <Router basename = { ProcessingInstruction.env.PUBLIC_URL }>
+
+      <TodoContainer />
+
+    </Router>
+
+  </React.StrictMode>,
+
+  document.getElementById( "root" )
+
+);
